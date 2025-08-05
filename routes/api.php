70ac86338363ef2b58d6f6813cfc11358d508896
Jan Eris Saludo <janerissaludo@gmail.com>
2025-08-06 06:41:57 +0800

    style: adjust indentation for product routes in api.php

--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,9 @@ use App\Domains\Pharmacy\Controllers\API\ProductController;
     Route::get('/users', [UserController::class, 'index'])->name('users.index');
 
     Route::prefix('products')->controller(ProductController::class)->group(function () {
-    Route::get('/', 'index');         // GET all products
-    Route::post('/', 'store');        // POST create a product
-    Route::get('{id}', 'show');       // GET single product
-    Route::put('{id}', 'update');     // PUT update product
-    Route::delete('{id}', 'destroy'); // DELETE product
-});
+        Route::get('/', 'index');         // GET all products
+        Route::post('/', 'store');        // POST create a product
+        Route::get('{id}', 'show');       // GET single product
+        Route::put('{id}', 'update');     // PUT update product
+        Route::delete('{id}', 'destroy'); // DELETE product
+    });
